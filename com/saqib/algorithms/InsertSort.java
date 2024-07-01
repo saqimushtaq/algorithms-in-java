2cbb4fb2c00b0f9db68ed9e2b55798a15ab51286
Saqib Mushtaq <i.saqibmushtaq@gmail.com>
2024-07-01 21:10:04 +0500

    insertion sort added

---
+++ com/saqib/algorithms/InsertSort.java
@@ -0,0 +1,24 @@
+package com.saqib.algorithms;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+        insertSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void insertSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
