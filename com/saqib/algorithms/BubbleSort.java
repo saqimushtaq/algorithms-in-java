d84bcd88492cdcdd5314a2ec32730ebdcbb4d8f3
Saqib Mushtaq <i.saqibmushtaq@gmail.com>
2024-07-01 17:56:39 +0500

    bubble sort added

---
+++ com/saqib/algorithms/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.saqib.algorithms;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+        bubbleSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped in the inner loop, then the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
